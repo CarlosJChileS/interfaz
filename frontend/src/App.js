c9277b14567c3a2da632e6743576d454266cdf87
Carlos Chile <150838094+CarlosJChileS@users.noreply.github.com>
2025-07-14 17:00:24 -0500

    Add help and report sections

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import EditProfilePage from './presentation/pages/EditProfilePage';
 import AdminPage from './presentation/pages/AdminPage';
 import HelpPage from './presentation/pages/HelpPage';
 import ContactPage from './presentation/pages/ContactPage';
+import Reportes from './presentation/pages/Reportes';
 import ProtectedRoute from './ProtectedRoute';
 import { AuthProvider } from './AuthContext';
 import { LanguageProvider } from './LanguageContext';
@@ -84,6 +85,7 @@ function App() {
               }
             />
             <Route path="/ayuda" element={<HelpPage />} />
+            <Route path="/reportes" element={<Reportes />} />
             <Route path="/contacto" element={<ContactPage />} />
           </Routes>
         </Router>
